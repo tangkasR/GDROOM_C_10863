7a2789cb7c3d021746691c1bfa18f29c375d5af1
tangkasR <tangkasrisdianto2@gmail.com>
2022-09-21 18:19:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gdroom_c_10863/room/NoteDao.kt
@@ -0,0 +1,19 @@
+package com.example.gdroom_c_10863.room
+
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+    @Insert
+    suspend fun addNote(note: Note)
+    @Update
+    suspend fun updateNote(note: Note)
+    @Delete
+    suspend fun deleteNote(note: Note)
+    @Query("SELECT * FROM note")
+    suspend fun getNotes() : List<Note>
+    @Query("SELECT * FROM note WHERE id =:note_id")
+    suspend fun getNote(note_id: Int) : List<Note>
+
+
+}
